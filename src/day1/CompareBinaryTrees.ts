87b3bdcec26b975065906af80f4a87b2b8ebf602
Chrystian Ferreira <chrystian.profissional@gmail.com>
2024-04-09 07:59:19 -0300

    feat: CompareBinaryTrees

--- src/day1/CompareBinaryTrees.ts
+++ src/day1/CompareBinaryTrees.ts
@@ -1,3 +1,9 @@
 export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+  // pre
+  if(a === null && b === null) return true;
+  if(a === null || b === null) return false;
+  if(a.value !== b.value) return false;
 
+  // recurse
+  return compare(a.left, b.left) && compare(a.right, b.right)
 }
